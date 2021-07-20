2e52ee8be10eb78687e43676e82b118e24344445
cooperrc <coopercutbirth@yahoo.com>
2021-07-19 23:08:35 -0500

    did gamefinder js and css

---
+++ client/src/pages/GameFinder/GameFinder.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+
+
+function PartPicker(){
+    return(
+        <>
+           <div className='wrapper'>
+     
+     </div>
+     <div className="container">
+       <div className="card">
+         <div className="imgBx">
+          {/*  <img src="https://cdn.freebiesupply.com/logos/thumbs/2x/minecraft-logo.png">
+         </div>*/}
+         </div>
+         
+         <div className="contentBx">
+           <h2>Game Display</h2>
+           <div className="size">
+             <h3>Systems :</h3>
+             <span>PC</span>
+             <span>PS4</span>
+             <span>XB</span>
+             <span>SW</span>
+           </div>
+         
+           <a href="#">Buy Now</a>
+         </div>
+       </div>
+     </div>
+     <div className ="container">
+       <div className="card">
+         <div className="imgBx">
+           {/*<img src="https://cdn.freebiesupply.com/logos/thumbs/2x/minecraft-logo.png" alt="https://cdn.freebiesupply.com/logos/thumbs/2x/minecraft-logo.png">
+         </div>*/}
+         </div>
+         <div className="contentBx">
+           <h2>Game Display</h2>
+           <div className="size">
+             <h3>Systems :</h3>
+             <span>PC</span>
+             <span>PS4</span>
+             <span>XB</span>
+             <span>SW</span>
+           </div>
+         
+           <a href="#">Buy Now</a>
+         </div>
+       </div>
+     </div>
+     <div className="container">
+       <div className="card">
+         <div className="imgBx">
+          {/* <img src="https://cdn.freebiesupply.com/logos/thumbs/2x/minecraft-logo.png">
+         </div>*/}
+         
+         <div className="contentBx">
+           <h2>Game Display</h2>
+           <div className="size">
+             <h3>Systems :</h3>
+             <span>PC</span>
+             <span>PS4</span>
+             <span>XB</span>
+             <span>SW</span>
+           </div>
+           
+         
+           <a href="#">Buy Now</a>
+         </div>
+       </div>
+     </div>
+     </div>
+    
+    
+     
+     
+        </>
+    );
+}
+
+export default PartPicker;
