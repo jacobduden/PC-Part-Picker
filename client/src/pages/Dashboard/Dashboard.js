b1075154faf4c12da52e049b9605550544d018fc
enzonsn <enzonsperduti@gmail.com>
2021-07-21 18:28:20 -0500

    fixed navbar, restyled dashboard and contents

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Dashboard.css';
 
 function Dashboard () {
     return (
         <>
-        <section className='desc'>
-            <h1> Looking for new games to play?</h1>
-            <h3> Here are some popular games! </h3>
+        <section className='home-main-sect'>
+            <Container fluid className="home-main-cont">
+                <Container className="home-main-cont2">
+                    <Row className="home-main-row">
+                        <Col className="home-main-col">
+                            <h1 className="head-main-name" style={{padding:20}}>Looking for new games to play?</h1>
+                            <h3 className="head-main-desc" style={{padding:50, textAlign: 'center'}}>Here are some popular games!</h3>
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>
+            
+            
+            <Container fluid className="home-bot-cont">
+                    <Container className="home-bot-cont2">
+                        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+                            <h1 className="home-bot--title">Browse a few genres!</h1>
+                        </Row>
+                        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+                            {/* <Col className="home-bot-card">
+                                <GameCard img="" title="" url="" />
+                            </Col>
+                            <Col className="home-bot-card">
+                                <GameCard img="" title="" url="" />
+                            </Col>  
+                            <Col className="home-bot-card">
+                                <GameCard img="" title="" url="" />
+                            </Col>  
+                            <Col className="home-bot-card">
+                                <GameCard img="" title="" url="" />
+                            </Col>  
+                            <Col className="home-bot-card">
+                                <GameCard img="" title="" url="" />
+                            </Col>  
+                            <Col className="home-bot-card">
+                                <GameCard img="" title="" url="" />
+                            </Col>   */}
+                        </Row>
+                    </Container>
+            </Container>
         </section>
         </>
     );
--- client/src/pages/assets/Navbar.js
+++ client/src/pages/assets/Navbar.js
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container';
 import {LinkContainer} from "react-router-bootstrap";
-
+import './Navbar.css';
 
 export default function NavBar(){
 
@@ -22,19 +22,19 @@ export default function NavBar(){
                     <Navbar.Toggle className="ml-auto" aria-controls="responsive-navbar-nav" onClick={()=>{ updateExpanded(expand ? false : "expanded");}}/>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="justify-content-center" style={{ flex: 1}} defaultActiveKey="#home">
-                            <LinkContainer to="/Trending">
-                                <Nav.Link onClick={() => updateExpanded(false)}>Trending</Nav.Link>
-                            </LinkContainer>a
-                            <LinkContainer to="/HotReleases">
-                                <Nav.Link onClick={() => updateExpanded(false)}>Hot Releases</Nav.Link>
+                            <LinkContainer to="/games">
+                                <Nav.Link onClick={() => updateExpanded(false)}>Games</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/playercard">
+                                <Nav.Link onClick={() => updateExpanded(false)}>Player Card</Nav.Link>
                             </LinkContainer>
 							<LinkContainer to="/">
 								<Nav.Item>
 									<Navbar.Brand className="mx-auto">Game Finder</Navbar.Brand>
 								</Nav.Item>
 							</LinkContainer>
-                            <LinkContainer to="/Mostplayed">
-                                <Nav.Link onClick={() => updateExpanded(false)}>Most Played</Nav.Link>
+                            <LinkContainer to="/wishlist">
+                                <Nav.Link onClick={() => updateExpanded(false)}>Wishlist</Nav.Link>
                             </LinkContainer>
                             <LinkContainer to="/contact">    
                                 <Nav.Link onClick={() => updateExpanded(false)}>Contact</Nav.Link>
