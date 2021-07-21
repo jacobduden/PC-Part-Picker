e834b455701f2abd6d3ba0772ba37ede29511bc6
enzonsn <enzonsperduti@gmail.com>
2021-07-21 16:52:34 -0500

    tiny revision to App css and js. revamp of navabar with react-bootstrap. and change of dashboard formating

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,19 @@ import Dashboard from './pages/Dashboard/Dashboard';
 
 function App(){
     return(
-        <>
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route path='/' exact component={Dashboard}/>
-                <Route path='/dashboard' component={Dashboard}/>
-                {/*<Route path='/Games' component={Games}/>
-                <Route path='/wishlist' component= {Wishlist}/>
-                <Route path='/Playercard' component= {Playercard}/>
-                <Route path='/Contact' component= {Contact}/> */}
-            </Switch>
-            {/*<Footer/>*/}
-        </Router>
-        </>
+        <div className="bg">
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path='/' exact component={Dashboard}/>
+                    {/*<Route path='/Games' component={Games}/>
+                    <Route path='/wishlist' component= {Wishlist}/>
+                    <Route path='/Playercard' component= {Playercard}/>
+                    <Route path='/Contact' component= {Contact}/> */}
+                </Switch>
+                {/*<Footer/>*/}
+            </Router>
+        </div>
     );
 }
 
--- client/src/pages/assets/Navbar.js
+++ client/src/pages/assets/Navbar.js
@@ -1,41 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'
+import React, {useState} from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from "react-router-bootstrap";
 
-function Navbar() {
+
+export default function NavBar(){
+
+    const [expand, updateExpanded] = useState(false);
+    const [navColour, updateNavbar] = useState(false);
+    function scrollHandler() {
+        if (window.scrollY >= 20) {updateNavbar(true);} 
+        else {updateNavbar(false);}
+    }
+    
+    window.addEventListener("scroll", scrollHandler);
+
     return (
-        <>
-            <nav className='navbar'>
-                <div className='navbarContainer'>
-                    <Link to='/dashboard' className='navbar-logo'>
-                        <i className='fab steam-square' /> Steam Overlook
-                    </Link>
-                    <ul className='navnav'>
-                      <li className='nav-item'>
-                        <Link to='/HotReleases' className='nav-links'>
-                          Hot Releases
-                        </Link>
-                      </li>
-                      <li className='nav-item'>
-                        <Link to='/Trending' className='nav-links'>
-                          Trending
-                        </Link>
-                      </li>
-                      <li className='nav-item'>
-                        <Link to='/Mostplayed' className='nav-links'>
-                          Most Played
-                        </Link>
-                      </li>
-                      <li className='nav-item'>
-                        <Link to='/Contact' className='nav-links'>
-                          Contact
-                        </Link>
-                      </li>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    )
+            <Navbar fixed="top" expand="md" expanded={expand} className={navColour ? "sticky" : "navbar"}>
+                <Container>
+                    <Navbar.Toggle className="ml-auto" aria-controls="responsive-navbar-nav" onClick={()=>{ updateExpanded(expand ? false : "expanded");}}/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="justify-content-center" style={{ flex: 1}} defaultActiveKey="#home">
+                            <LinkContainer to="/Trending">
+                                <Nav.Link onClick={() => updateExpanded(false)}>Trending</Nav.Link>
+                            </LinkContainer>a
+                            <LinkContainer to="/HotReleases">
+                                <Nav.Link onClick={() => updateExpanded(false)}>Hot Releases</Nav.Link>
+                            </LinkContainer>
+							<LinkContainer to="/">
+								<Nav.Item>
+									<Navbar.Brand className="mx-auto">Game Finder</Navbar.Brand>
+								</Nav.Item>
+							</LinkContainer>
+                            <LinkContainer to="/Mostplayed">
+                                <Nav.Link onClick={() => updateExpanded(false)}>Most Played</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/contact">    
+                                <Nav.Link onClick={() => updateExpanded(false)}>Contact</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+    );
 }
-
-export default Navbar;
