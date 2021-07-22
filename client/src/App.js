a6ebe26d8a0f7af3bbdf9f8f7fc8486335ba323d
enzonsn <enzonsperduti@gmail.com>
2021-07-22 15:08:07 -0500

    changed action category image

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ function App(){
                     <Route path='/Contact' component= {Contact}/>
                     <Route path='/PrivacyPolicy' component= {PrivacyPolicy}/>
                     <Route path='/Login' component = {Login}/>
+                    <Route path='/underConst' component = {UnderConst}/>
                 </Switch>
                 <Footer/> 
             </Router>
