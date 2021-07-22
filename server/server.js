3f9af7865abc8696b33bb4a35e0ceb407fd6d695
jacobduden <75395688+jacobduden@users.noreply.github.com>
2021-07-22 18:19:22 -0500

    added MONGODB_URI config

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const db = require('./config/connection');
+const db = require('./config/connection.js');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas/index');
 const { authMiddleware } = require('./utils/Auth');
 
 const app = express();
-const PORT = 3001;
+const PORT = proccess.env.MONGODB_URI || 3001;
 
 
 const server = new ApolloServer({
