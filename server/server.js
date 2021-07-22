813535413f7c46d9507edec64a0a796db8b71b9e
jacobduden <75395688+jacobduden@users.noreply.github.com>
2021-07-22 16:28:28 -0500

    fixing errors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas/index');
