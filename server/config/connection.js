6b40e56c7cf7126f0991c929973125a207175e09
jacobduden <75395688+jacobduden@users.noreply.github.com>
2021-07-26 16:15:20 -0500

    more heroku errors

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 const uri = procces.env.MONGODB_URI
-mongoose.connect("mongodb://127.0.0.1:27017/gamefndr" || uri,{
+mongoose.connect("mongodb://127.0.0.1:27017/gamefndr",{
+        uri: uri,
         useCreateIndex:true,
         useNewUrlParser: true,
         useUnifiedTopology: true}).then(()=> {
